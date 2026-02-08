e4266bc7cd54ad543b3b4eaa044f46b90522630b
Wasif Hasan <wasifhasancse@gmail.com>
2026-02-08 23:22:38 +0600

    sovle problem 2 using ternary operator

--- problem-2.js
+++ problem-2.js
@@ -1,19 +1,23 @@
 // OTP Validation 
+// function validOtp(otp) {
+//   // Your code here
+//   if (typeof otp === "string") {
+//     if (otp.length === 8 && otp.startsWith("ph-")) {
+//       return true;
+//     }
+//     return false;
+//   }
+//   return "Invalid";
+// }
+// using ternary operator
 function validOtp(otp) {
-  // Your code here
-  if (typeof otp === "string") {
-    if (otp.length === 8 && otp.startsWith("ph-")) {
-      return true;
-    }
-    return false;
-  }
-  return "Invalid";
+  return typeof otp === 'string' ? otp.length === 8 && otp.startsWith('ph-') ? true : false : 'Invalid';
 }
 
 console.log(validOtp("ph-10985")); // true
 console.log(validOtp("ph-1234")); // false
 console.log(validOtp("abc-12345")); // false
-console.log(validOtp("ph-10985")); // Invalid
+console.log(validOtp("Ph-10985")); // Invalid
 console.log(validOtp(12345678)); // Invalid
 
 // Test Cases:
